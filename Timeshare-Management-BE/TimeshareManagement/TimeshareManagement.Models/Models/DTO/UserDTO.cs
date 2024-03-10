4a1bd68f56d2cfae496a6a36b89e40d1b57a8b95
HoangNam2811 <86917786+nguyen-hoang-Nnam@users.noreply.github.com>
2024-03-10 10:15:30 +0700

    createUser

--- Timeshare-Management-BE/TimeshareManagement/TimeshareManagement.Models/Models/DTO/UserDTO.cs
+++ Timeshare-Management-BE/TimeshareManagement/TimeshareManagement.Models/Models/DTO/UserDTO.cs
@@ -10,10 +10,10 @@ namespace TimeshareManagement.Models.Models.DTO
     {
         public string? Id { get; set; }
         public string? Name { get; set; }
-        /*public string? LastName { get; set; }*/
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public string? Password { get; set; }
         public bool isActive { get; set; }
     }
 }
--- Timeshare-Management-BE/TimeshareManagement/TimeshareManagement.Models/obj/Debug/net7.0/TimeshareManagement.Models.AssemblyInfo.cs
+++ Timeshare-Management-BE/TimeshareManagement/TimeshareManagement.Models/obj/Debug/net7.0/TimeshareManagement.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeshareManagement.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+491ed2f72c74ba680c6a75879db8d7310a95c984")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d2e0e4b37f2cb988e57265daf24241f787c838")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeshareManagement.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeshareManagement.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
